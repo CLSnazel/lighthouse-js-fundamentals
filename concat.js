863d8826d68cd3d7152ff9da569720ae0a4dcf5f
Chantal Snazel <clsnazel@gmail.com>
2020-08-23 01:27:40 +0000

    Completed all code eval activities

---
+++ concat.js
@@ -0,0 +1,12 @@
+function concat(arr1, arr2) {
+  result = arr1;
+  for(let i = 0; i < arr2.length; i++){
+    result.push(arr2[i]);
+  }
+  return result;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
